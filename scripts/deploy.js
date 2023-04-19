f9c78ed1fa322985f9ea25255674c2f089c8a0f5
petar <cvaredp@gmail.com>
2023-04-19 20:30:05 +0200

    inital

---
+++ scripts/deploy.js
@@ -0,0 +1,45 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require("hardhat");
+const fs = require("fs");
+
+async function main() {
+  //const Greeter = await hre.ethers.getContractFactory("Greeter");
+  //const greeter = await Greeter.deploy("Hello, Hardhat!");
+
+  //const NTF = await hre.ethers.getContractFactory("ElfakNFT");
+  //const nft = await NTF.deploy();
+  const taxFee = 5;
+  const CROWDFUND = await hre.ethers.getContractFactory("CrowdFund");
+  const crowdFund = await CROWDFUND.deploy(taxFee);
+
+  await crowdFund.deployed();
+
+  const address = JSON.stringify({ address: crowdFund.address }, null, 4);
+  fs.writeFile(
+    "./src/artifacts/contractAddress.json",
+    address,
+    "utf8",
+    (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log("Deployed contract address", crowdFund.address);
+    }
+  );
+  //await greeter.deployed();
+
+  //console.log("Greeter deployed to:", greeter.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
